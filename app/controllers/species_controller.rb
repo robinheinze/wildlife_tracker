7aea270919aa3a9f11ee2e74cb58834c1025eef5
robin miller & Maureen Dugan <student@epicodus.com>
2014-04-01 10:52:03 -0700

    adds completed species CRUD functionality

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -10,6 +10,20 @@ class SpeciesController < ApplicationController
     render('species/show.html.erb')
   end
 
+  def new
+    @species = Species.new
+    render('species/new.html.erb')
+  end
+
+  def create
+    @species = Species.new(params[:species])
+    if @species.save
+      render('species/success.html.erb')
+    else
+      render('species/new.html.erb')
+    end
+  end
+
   def edit
     @species = Species.find(params[:id])
     render('species/edit.html.erb')
@@ -22,6 +36,12 @@ class SpeciesController < ApplicationController
     else
       render('species/edit.html.erb')
     end
+  end
+
+  def destroy
+    @species = Species.find(params[:id])
+    @species.destroy
+    render('species/destroy.html.erb')
   end
 
 end
